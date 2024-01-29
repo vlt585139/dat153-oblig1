9cf59277157911b1fb91bbf8805a03d15d759ba3
Ivar H Nesheim <89256274+Ivhene@users.noreply.github.com>
2024-01-29 14:06:04 +0100

    feat: started adding images to application (#4)

--- app/src/main/java/com/example/quizappassignment1/GalleryActivity.java
+++ app/src/main/java/com/example/quizappassignment1/GalleryActivity.java
@@ -14,5 +14,7 @@ public class GalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.temp);
+
+        Log.d("Test", MainActivity.optionList.toString());
     }
 }
