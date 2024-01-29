4796c4ed26cc0d66daac2e12d52ec252512ebbe7
Ivar H Nesheim <89256274+Ivhene@users.noreply.github.com>
2024-01-29 13:04:12 +0100

    Feat/button to gallery activity (#2)
    * Initial Commit
    
    * feat: added button to Gallery Activity

---
+++ app/src/main/java/com/example/quizappassignment1/GalleryActivity.java
@@ -0,0 +1,18 @@
+package com.example.quizappassignment1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.temp);
+    }
+}
--- app/src/main/java/com/example/quizappassignment1/MainActivity.java
+++ app/src/main/java/com/example/quizappassignment1/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.quizappassignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button openGalleryButton = (Button) findViewById(R.id.openGallery);
+        openGalleryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", "Open Gallery");
+                Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
